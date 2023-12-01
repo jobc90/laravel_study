a0959fce100d25bd95f01888f9853c2e99c39b5f
조병철 <jobc90@gmail.com>
2023-12-01 14:21:50 +0900

    에러 메세지 표시

--- example-app/resources/views/articles/create.blade.php
+++ example-app/resources/views/articles/create.blade.php
@@ -8,9 +8,12 @@
             <form action="/articles" method="POST" class="mt-3">
                 @csrf
                 <input type="text" class="block w-full mb-2 rounded">
+                @error('body')
+                    <p class="text-xs text-red-500 mb-3">{{ $message }}</p>
+                @enderror
                 <button class="py-1 px-3 bg-black text-white rounded text-xs">저장하기</button>
             </form>
-            {{ dd($errors->all()) }}
+            {{-- {{ dd($errors->all('body')) }} --}}
         </div>
     </body>
 </html>
