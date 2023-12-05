c28db8de743b4d9ad1c5498cd544425e42a42e6b
조병철 <jobc90@gmail.com>
2023-12-05 18:16:51 +0900

    DB연결

--- example-app/resources/views/articles/create.blade.php
+++ example-app/resources/views/articles/create.blade.php
@@ -7,13 +7,14 @@
             <h1 class="text-2xl">글쓰기</h1>
             <form action="/articles" method="POST" class="mt-3">
                 @csrf
-                <input type="text" class="block w-full mb-2 rounded">
+                <input type="text" class="block w-full mb-2 rounded" value="{{ old('body') }}">
                 @error('body')
-                    <p class="text-xs text-red-500 mb-3">{{ $message }}</p>
+                    <p class="text-xs text-red-600 mb-3"> {{ $message }} </p>
                 @enderror
                 <button class="py-1 px-3 bg-black text-white rounded text-xs">저장하기</button>
             </form>
             {{-- {{ dd($errors->all('body')) }} --}}
+            {{-- {{ dd(old('body'))}} --}}
         </div>
     </body>
 </html>
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\Article;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -37,7 +40,7 @@ Route::get('/articles/create', function () {
 
 Route::post('/articles', function (Request $request) {
     // 비어있지않고, 문자열이고, 255자를 넘으면 안된다.
-    $request->validate([
+    $input = $request->validate([
         'body' => [
             'required',
             'string',
@@ -45,6 +48,48 @@ Route::post('/articles', function (Request $request) {
         ],
     ]);
 
+    // //글을 저장한다.
+
+    // //PDO를 이용하는 방법
+    // $host = config('database.connections.mysql.host');
+    // $dbname = config("database.connections.mysql.database");
+    // $username = config('database.connections.mysql.username');
+    // $password = config("database.connections.mysql.password");
+    // // pdo 객체를 만들고
+    // $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    // // 쿼리 준비
+    // $stmt = $conn->prepare("INSERT INTO articles (body, user_id) VALUES (:body, :userId)");
+
+    // // dd($request->all());
+    // // dd($request->collect());
+    // // $body = $request->input('body');
+    // // 쿼리 값을 설정
+    // $stmt->bindValue(':body', $input['body']);
+    // // $stmt->bindValue(':userId', $request->user()->id);
+    // $stmt->bindValue(':userId', Auth::id());
+    // // 실행
+    // $stmt->execute();
+
+    // // DB 파사드를 이용하는 방법
+    // DB::statement("INSERT INTO articles (body, user_id) VALUES (:body, :userId)", ['body' => $input['body'], 'userId' => Auth::id()]);
+    
+    // // 쿼리 빌더를 사용하는 방법
+    // DB::table('articles')->insert([
+    //     'body' => $input['body'],
+    //     'user_id' => Auth::id()
+    // ]);
+
+    // // 엘로퀀트 ORM을 사용한는 방법
+    // $article = new Article;
+    // $article->body = $input['body'];
+    // $article->user_id = Auth::id();
+    // $article->save();
+
+    // 엘로퀀트 ORM을 사용한는 방법
+    Article::create([
+        'body' => $input['body'],
+        'user_id' => Auth::id()
+    ]);
 
     return 'hello';
 });
