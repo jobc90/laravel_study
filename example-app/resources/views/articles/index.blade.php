ef3b86f0fe53b49400701575e3d2331f349ba07e
조병철 <jobc90@gmail.com>
2023-12-20 17:26:52 +0900

    유저 프로필화면(컴포넌트에 데이터 넘겨주기)

--- example-app/resources/views/articles/index.blade.php
+++ example-app/resources/views/articles/index.blade.php
@@ -6,21 +6,7 @@
     </x-slot>
     <div class="container p-5 mx-auto">
         @foreach($articles as $article)
-            <div class="background-white border rounded mb-3 p-3">
-                <p>{{ $article->body }}</p>
-                {{-- <p>{{ $article->created_at->format('Y년 m월 d일 H시') }}</p> --}}
-                <p>{{ $article->user->name }}</p>
-                <p class="text-sm text-gray-500">
-                    <a href="{{ route('articles.show', ['article'=>$article->id]) }}">{{ $article->created_at->diffForHumans() }}
-                        <span class="ml-2">   
-                            댓글 {{ $article->comments_count}} 
-                            @if ($article->recent_comments_exists) (new!)@endif
-                        </span>
-                    </a>
-                </p>
-                
-                <x-article-button-group :article=$article />
-            </div>
+            <x-list-article-item :article=$article />
         @endforeach
 
         {{-- @for($i=0; $i < $articles->count(); $i++)
---
+++ example-app/resources/views/components/list-article-item.blade.php
@@ -0,0 +1,19 @@
+<div class="background-white border rounded mb-3 p-3">
+    <p>{{ $article->body }}</p>
+    {{-- <p>{{ $article->created_at->format('Y년 m월 d일 H시') }}</p> --}}
+    <p>
+        <a href="{{ route('profile', ['user' => $article->user->id]) }}">
+            {{ $article->user->name }}
+        </a>
+    </p>
+    <p class="text-sm text-gray-500">
+        <a href="{{ route('articles.show', ['article'=>$article->id]) }}">{{ $article->created_at->diffForHumans() }}
+            <span class="ml-2">   
+                댓글 {{ $article->comments_count}} 
+                @if ($article->recent_comments_exists) (new!)@endif
+            </span>
+        </a>
+    </p>
+    
+    <x-article-button-group :article=$article />
+</div>
