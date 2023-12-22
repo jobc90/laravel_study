b46c9ce363c791fb3b2968265a39c3f987345f1c
조병철 <jobc90@gmail.com>
2023-12-22 11:45:47 +0900

    다대다관계(유저 팔로우기능)

--- example-app/resources/views/profile/show.blade.php
+++ example-app/resources/views/profile/show.blade.php
@@ -2,8 +2,27 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <h1 class="text-center text-5xl ">{{ $user->name }}</h1>
-            <div class="text-center text-2xl">
-                게시물 {{ $user->articles->count() }}
+            <div class="flex flex-row place-content-center">
+                <div class="text-center text-2xl mr-5">
+                    게시물 {{ $user->articles->count() }}
+                    구독자 {{ $user->followers->count() }}
+                </div>
+            @if(Auth::id() != $user->id)
+                <div>
+                    @if (Auth::user()->isFollowing($user))
+                        <form method="POST" action="{{ route('unfollow', ['user' => $user->username]) }}">
+                            @csrf
+                            @method('delete')
+                            <x-danger-button>구독취소</x-danger-button>
+                        </form>
+                    @else
+                        <form method="POST" action="{{ route('follow', ['user' => $user->username]) }}">
+                            @csrf
+                            <x-primary-button>구독하기</x-primary-button>
+                        </form>
+                    @endif
+                </div>
+            @endif
             </div>
             <div>
                 @forelse ($user->articles as $article)
